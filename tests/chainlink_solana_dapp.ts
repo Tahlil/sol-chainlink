4fd215131e8e329bef93fd9eeed27cbfab8632fc
tahlil <bsse0803@iit.du.ac.bd>
2022-06-29 00:51:00 +0600

    Add chainlink program id

--- tests/chainlink_solana_dapp.ts
+++ tests/chainlink_solana_dapp.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@project-serum/anchor";
 
 const CHAINLINK_FEED = "HgTtcbcmp5BeThax5AU8vg4VwK79qAvAKKFMs8txMLW6"; 
+const CHAINLINK_PROGRAM_ID = "";
+
 describe("chainlink_solana_dapp", () => {
 
 });
