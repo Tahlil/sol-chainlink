961fea509e168dcfa2980d408c3e98783f1ca32c
tahlil <bsse0803@iit.du.ac.bd>
2022-06-28 19:44:52 +0600

    Add execute context

--- programs/chainlink_solana_dapp/src/lib.rs
+++ programs/chainlink_solana_dapp/src/lib.rs
@@ -17,3 +17,13 @@ pub mod chainlink_solana_dapp {
     
 }
 
+#[derive(Accounts)]
+pub struct Execute<'info> {
+    #[account(init, payer=user, space=100)]
+    let result_account: Account<'info, ResultAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub chainlink_program: AccountInfo<'info>,
+    
+}
