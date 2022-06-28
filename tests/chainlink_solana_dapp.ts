5b31dcee016bc1cb74ccdda9902b51cb811c0ffd
tahlil <bsse0803@iit.du.ac.bd>
2022-06-29 00:58:10 +0600

    Run execute in test

--- tests/chainlink_solana_dapp.ts
+++ tests/chainlink_solana_dapp.ts
@@ -4,5 +4,19 @@ const CHAINLINK_FEED = "HgTtcbcmp5BeThax5AU8vg4VwK79qAvAKKFMs8txMLW6";
 const CHAINLINK_PROGRAM_ID = "";
 
 describe("chainlink_solana_dapp", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.chainlink_solana_dapp
+  it('Queries SOL/USD Price feed', async() => {
+    const resultAccount  = anchor.web3.Keypair.generate();
+    await program.rpc.execute({
+      accounts: {
+        resultAccount: resultAccount.publicKey,
+        user: provider.wallet.publicKey,
+        chainlinkFeed: CHAINLINK_FEED,
+        chainlinkProgram: CHAINLINK_PROGRAM_ID
 
+      }
+    })  
+  })
 });
