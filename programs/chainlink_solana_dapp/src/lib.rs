b3e57ee58cd2ec7b9b841384cf4367cfcc6368a0
tahlil <bsse0803@iit.du.ac.bd>
2022-06-28 19:39:13 +0600

    Add execute function

--- programs/chainlink_solana_dapp/src/lib.rs
+++ programs/chainlink_solana_dapp/src/lib.rs
@@ -7,6 +7,13 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod chainlink_solana_dapp {
     use super::*;
 
+    pub fn execute(ctx: Contexr<Execute>) -> ProgramResult {
+        let round = chainlink::latest_round_data(ctx.accounts.chainlink_program.to_account_info(),
+        ctx.accounts.chainlink_feed.to_account_info());
+        let result_account = &mut ctx.accounts.result_account;
+        result_account.value = round.answer;
+        Ok(());
+    }
     
 }
 
