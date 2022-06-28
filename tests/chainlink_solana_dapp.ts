6eae4c1ee88bcf98c30328a906390f6ce1f84bb3
tahlil <bsse0803@iit.du.ac.bd>
2022-06-29 01:02:05 +0600

    Copy program ID

--- tests/chainlink_solana_dapp.ts
+++ tests/chainlink_solana_dapp.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 
 const CHAINLINK_FEED = "HgTtcbcmp5BeThax5AU8vg4VwK79qAvAKKFMs8txMLW6"; 
-const CHAINLINK_PROGRAM_ID = "";
+const CHAINLINK_PROGRAM_ID = "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny";
 
 describe("chainlink_solana_dapp", () => {
   const provider = anchor.AnchorProvider.env();
