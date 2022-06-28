1bb7741cbb89883c45a92de6156d54c1b63ebd08
tahlil <bsse0803@iit.du.ac.bd>
2022-06-28 19:34:16 +0600

    Remove some boilerplate code

--- programs/chainlink_solana_dapp/src/lib.rs
+++ programs/chainlink_solana_dapp/src/lib.rs
@@ -6,10 +6,6 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod chainlink_solana_dapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+    
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
