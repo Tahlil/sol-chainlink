4e41b83cbecb282b1023f31c0c9b7eb59edc019a
tahlil <bsse0803@iit.du.ac.bd>
2022-06-29 01:13:16 +0600

    Import programresult

--- programs/chainlink_solana_dapp/src/lib.rs
+++ programs/chainlink_solana_dapp/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use chainlink_solana as chainlink;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
